7c6fb9b418978ad9ba752154745fdf5116a28bf5
Omer Schloss <schlossomer@gmail.com>
2023-06-29 11:54:00 +0300

    unneccesary imports

--- jobScraper_Google.py
+++ jobScraper_Google.py
@@ -1,8 +1,7 @@
 import pandas as pd
 
 import urllib.parse
-from bs4 import BeautifulSoup
-import os
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
